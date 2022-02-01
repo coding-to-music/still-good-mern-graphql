b853214817242079b94a4d95497414f643797da4
TJ James <jamestw13@gmail.com>
2022-02-01 00:09:32 -0600

    started material ui AppBar component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, MenuItem } from '@mui/material';
 
 function Header() {
   return (
-    <div>
-      <h2>Header/Navbar section</h2>
-      <Link to="/"> Welcome </Link>
-      <Link to="/join"> Join </Link>
-      <Link to="/itemlist"> ItemList </Link>
-      <Link to="/itemedit"> ItemEdit </Link>
-    </div>
+    <AppBar>
+      <h2>StillGood</h2>
+      <Toolbar disableGutters="true">
+        <MenuItem component={Link} to="/">
+          Welcome
+        </MenuItem>
+        <MenuItem component={Link} to="/join">
+          Join
+        </MenuItem>
+        <MenuItem component={Link} to="/itemlist">
+          ItemList
+        </MenuItem>
+        <MenuItem component={Link} to="/itemedit">
+          ItemEdit
+        </MenuItem>
+      </Toolbar>
+    </AppBar>
   );
 }
 export default Header;
