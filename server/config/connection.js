3b0c114b6951f2baf7d8a0ed05bac9ba6c3939a1
unknown <alexm1397@gmail.com>
2022-02-01 20:11:05 -0600

    added db name to connection.js and created model index

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //needs DB name
-mongoose.connect(process.env.MONGODB_URI || '', {
+mongoose.connect(process.env.MONGODB_URI || 'still-good-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
