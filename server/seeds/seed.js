fbba80dc662bb2a2ae2b084398bf08fd4e352144
Victor Weinert <dev@vweinert.com>
2022-02-04 16:55:59 -0600

    working on seeds, some hardcoded usernames are included, but most are random

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,18 +1,24 @@
-const db = require('./connection');
-const { Item } = require('../models');
-
+const db = require('../config/connection');
+const { Item, User } = require('../models');
+const { faker } = require('@faker-js/faker');
 db.once('open', async () => {
   await Item.deleteMany();
+  await User.deleteMany();
+  const userData = [];
+  userData.push({email: "email@email.com",password: "password"});
+  for (let i = 0; i < 50; i += 1) {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    
+    userData.push({email, password});
+  }
 
-  const item = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
-  ]);
+  const createdUsers = await User.collection.insertMany(userData);
+  const createdItems = []
+  for (let i = 0; i < 150; i += 1) {
 
-  console.log('categories seeded');
-
+    
+    createdItems.push({});
+  }
   process.exit();
 });
