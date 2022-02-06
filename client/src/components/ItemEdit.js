db2022412414096299cf338e85a89c8824c200be
unknown <jamestw13@gmail.com>
2022-02-06 13:20:46 -0600

    added UI validation for ItemEdit

--- client/src/components/ItemEdit.js
+++ client/src/components/ItemEdit.js
@@ -16,173 +16,190 @@ import AddTaskIcon from '@mui/icons-material/AddTask';
 import DoDisturb from '@mui/icons-material/DoDisturb';
 
 function ItemEdit({ dialogOpen, setEditedItem, editedItem, setDialogOpen }) {
-  // // Generic onChange handler
-  // function editField(event) {
-  //   const { name, value } = event.target;
-  //   setEditedItem({
-  //     ...editedItem,
-  //     [name]: value,
-  //   });
-  // }
-
-  // TODO Create validation for required fields
-  // Have been checking out this video for reference: https://www.youtube.com/watch?v=sTdt2cJS2dg
-  const [itemName, setItemName] = useState('');
-  const [useByDate, setUseByDate] = useState('');
+  // Generic onChange handler
+  function editField(event) {
+    const { name, value } = event.target;
+    setEditedItem({
+      ...editedItem,
+      [name]: value,
+    });
+  }
+  
+  // Item state for item name and useByDate
   const [itemNameError, setItemNameError] = useState(false);
   const [useByDateError, setUseByDateError] = useState(false);
-
-  // TODO Submit button handler
-  const handleFormSubmit = e => {
-    e.preventDefault();
+  
+  
+  // Submit item handler
+  const handleFormSubmit = event => {
+    event.preventDefault();
+    
+    // reset validation errors
     setItemNameError(false);
     setUseByDateError(false);
-
-    if (itemName === '') {
+    
+    // Show error if no name entered
+    if (!editedItem.name) {
       setItemNameError(true);
     }
-    if (itemName === '') {
+    
+    // Show error if no useByDate entered
+    if (!editedItem.useByDate) {
       setUseByDateError(true);
     }
+    
+    // If required fields are entered send data and reset form
+    if (editedItem.name && editedItem.useByDate) {
+      // reset validation errors
+      setItemNameError(false);
+      setUseByDateError(false);
+      // TODO useMutation saveItem
+      
+      // TODO check if mutation was successful and then reset edited item to nothing and close the modal
 
-    if (itemName && useByDate) {
-      console.log(itemName);
+      // clear edited
+      setEditedItem({});
+      
+      setDialogOpen(false);
+      
     }
-    // TODO useMutation saveItem
-
-    // clear edited
-    setEditedItem({});
-
-    setDialogOpen(false);
   };
   // TODO Submit and add button handler
   function handleSubmitAndAdd() {
     // TODO useMutation saveItem
-
+    
     // clear edited
     setEditedItem({});
   }
-
+  
   // Cancel button handler
   function handleEditCancel() {
     setEditedItem({});
     setDialogOpen(false);
   }
-
-  function handleClose() {}
+  
+  function handleClose() {
+    setDialogOpen(false)
+  }
   return (
     <Dialog onClose={handleClose} open={dialogOpen}>
-      <DialogTitle>Add/Edit Items</DialogTitle>
-      <form noValidate autoComplete="off" onSubmit={handleFormSubmit}>
-        <Stack margin={2} spacing={2}>
-          {/* Name Field */}
-          <TextField
-            name="name"
-            defaultValue={editedItem.name}
-            size="small"
-            label="Item"
-            type="text"
-            required
-            error={itemNameError}
-            // value={item.name}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-
-          <Grid container>
-            <Grid item xs={7}>
-              {/* Quantity Field */}
-              <TextField
-                name="quantity"
-                defaultValue={editedItem.quantity}
-                size="small"
-                label="Quantity"
-                type="number"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={5}>
-              {/* Unit Field */}
-              {/*
-              // TODO make this a select instead of a text field
-              // TODO create select options
-              */}
-              <TextField
-                name="unit"
-                defaultValue={editedItem.unit}
-                size="small"
-                label="Unit"
-                type="text"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-          </Grid>
-          {/* Use By Field */}
-          <TextField
-            name="useByDate"
-            defaultValue={editedItem.useByDate}
-            size="small"
-            label="Use by"
-            type="date"
-            required
-            error={useByDateError}
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-
-          {/* Added On Field */}
-          <TextField
-            name="addedDate"
-            defaultValue={editedItem.addedDate}
-            size="small"
-            label="Added on"
-            type="date"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-
-          {/* Storage Location Selector */}
-          <Select
-            name="storageLocation"
-            label="Stored where"
-            size="small"
-            value={editedItem.storageLocation ? editedItem.storageLocation : 'other'}
-          >
-            <MenuItem value="fridge">Fridge</MenuItem>
-            <MenuItem value="freezer">Freezer</MenuItem>
-            <MenuItem value="pantry">Pantry</MenuItem>
-            <MenuItem value="other">Other</MenuItem>
-          </Select>
-
-          {/* Buttons */}
-          <ButtonGroup variant="contained" fullWidth={true}>
-            <Tooltip title="Submit" placement="top">
-              <Button onClick={handleFormSubmit}>
-                <TaskAlt />
-              </Button>
-            </Tooltip>
-            <Tooltip title="Submit and Add" placement="top">
-              <Button onClick={handleSubmitAndAdd}>
-                <AddTaskIcon />
-              </Button>
-            </Tooltip>
-            <Tooltip title="Cancel" placement="top">
-              <Button onClick={handleEditCancel}>
-                <DoDisturb />
-              </Button>
-            </Tooltip>
-          </ButtonGroup>
-        </Stack>
-      </form>
+    <DialogTitle>Add/Edit Items</DialogTitle>
+    <form noValidate autoComplete="off" onSubmit={handleFormSubmit}>
+    <Stack margin={2} spacing={2}>
+    {/* Name Field */}
+    <TextField
+    name="name"
+    defaultValue={editedItem.name}
+    size="small"
+    label="Item"
+    type="text"
+    required
+    error={itemNameError}
+    InputLabelProps={{
+      shrink: true,
+    }}
+    onChange={(e) => editField(e)}
+    />
+    
+    <Grid container>
+    <Grid item xs={7}>
+    {/* Quantity Field */}
+    <TextField
+    name="quantity"
+    defaultValue={editedItem.quantity}
+    size="small"
+    label="Quantity"
+    type="number"
+    InputLabelProps={{
+      shrink: true,
+    }}
+    onChange={(e) => editField(e)}
+    />
+    </Grid>
+    <Grid item xs={5}>
+    {/* Unit Field */}
+    {/*
+      // TODO make this a select instead of a text field
+      // TODO create select options
+    */}
+    <TextField
+    name="unit"
+    defaultValue={editedItem.unit}
+    size="small"
+    label="Unit"
+    type="text"
+    InputLabelProps={{
+      shrink: true,
+    }}
+    onChange={(e) => editField(e)}
+    />
+    </Grid>
+    </Grid>
+    {/* Use By Field */}
+    <TextField
+    name="useByDate"
+    defaultValue={editedItem.useByDate}
+    size="small"
+    label="Use by"
+    type="date"
+    required
+    error={useByDateError}
+    InputLabelProps={{
+      shrink: true,
+    }}
+    onChange={(e) => editField(e)}
+    />
+    
+    {/* Added On Field */}
+    <TextField
+    name="addedDate"
+    defaultValue={editedItem.addedDate}
+    size="small"
+    label="Added on"
+    type="date"
+    InputLabelProps={{
+      shrink: true,
+    }}
+    onChange={(e) => editField(e)}
+    />
+    
+    {/* Storage Location Selector */}
+    <Select
+    name="storageLocation"
+    label="Stored where"
+    size="small"
+    value={editedItem.storageLocation ? editedItem.storageLocation : 'other'}
+    onChange={(e) => editField(e)}
+    >
+    <MenuItem value="fridge">Fridge</MenuItem>
+    <MenuItem value="freezer">Freezer</MenuItem>
+    <MenuItem value="pantry">Pantry</MenuItem>
+    <MenuItem value="other">Other</MenuItem>
+    </Select>
+    
+    {/* Buttons */}
+    <ButtonGroup variant="contained" fullWidth={true}>
+    <Tooltip title="Submit" placement="top">
+    <Button onClick={handleFormSubmit}>
+    <TaskAlt />
+    </Button>
+    </Tooltip>
+    <Tooltip title="Submit and Add" placement="top">
+    <Button onClick={handleSubmitAndAdd}>
+    <AddTaskIcon />
+    </Button>
+    </Tooltip>
+    <Tooltip title="Cancel" placement="top">
+    <Button onClick={handleEditCancel}>
+    <DoDisturb />
+    </Button>
+    </Tooltip>
+    </ButtonGroup>
+    </Stack>
+    </form>
     </Dialog>
-  );
-}
-
-export default ItemEdit;
+    );
+  }
+  
+  export default ItemEdit;
+  
