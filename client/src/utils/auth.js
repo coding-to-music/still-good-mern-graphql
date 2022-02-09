143c02491d22d39998b0c2a1abf106d71bcc9b68
TJ James <jamestw13@gmail.com>
2022-02-09 16:01:52 -0600

    login and signup automatically redirects to ItemList

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -30,6 +30,8 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
+
+    window.location.assign('/');
   }
 
   logout() {
