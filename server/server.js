fce4615fea6bbbba2143eb27863f7985d2659211
Victor Weinert <dev@vweinert.com>
2022-02-10 11:30:10 -0600

    VW working in laurens branch, added special rule in server.js to look for servicerworker.js

--- server/server.js
+++ server/server.js
@@ -34,7 +34,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+// Special rule for service worker
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
+});
 // catch any other req's send to homepage
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
